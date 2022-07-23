3dbc7e6d3a37e2a5a31b16124fbb60d43e1081c9
Tomek Uzarowski <tomek.uzarowski@gmail.com>
2022-07-23 20:51:36 +0200

    Add new section OurTeam

--- wp-content/themes/appwise/index.php
+++ wp-content/themes/appwise/index.php
@@ -116,4 +116,54 @@
     <a href="tel:728380638" class="CTABanner__button">728 380 638</a>
 </section>
 
+<section class="OurTeam">
+    <div class="Container">
+        <h2 class="OurTeam__heading TwoWeights HeadingWithBg"><?php _e('Zespół naszych <span>specjalistów</span>', 'appwise'); ?></h2>
+
+        <p class="OurTeam__subheading"><?php _e('Nasz zespół tworzą kreatywni,profesjonalni technicy dentystyczni, którzy latami zbierali doświadczenie w renomowanych pracowniach i ciągle są nastawieni rozwój swoich umiejętności.
+', 'appwise'); ?></p>
+
+        <div class="OurTeam__cards">
+            <div class="OurTeam__card">
+                <figure>
+                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/team-placeholder.png" alt="Person"></a>
+                </figure>
+
+                <a href="#">
+                    <h3 class="OurTeam__name">dr. Jan <span>Kowalski</span></h3>
+                </a>
+
+                <p class="OurTeam__position">Dyrektor placówki</p>
+            </div>
+            <div class="OurTeam__card">
+                <figure>
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/team-placeholder.png" alt="Person">
+                </figure>
+
+                <h3 class="OurTeam__name">dr. Jan <span>Kowalski</span></h3>
+
+                <p class="OurTeam__position">Dyrektor placówki</p>
+            </div>
+            <div class="OurTeam__card">
+                <figure>
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/team-placeholder.png" alt="Person">
+                </figure>
+
+                <h3 class="OurTeam__name">dr. Jan <span>Kowalski</span></h3>
+
+                <p class="OurTeam__position">Dyrektor placówki</p>
+            </div>
+            <div class="OurTeam__card">
+                <figure>
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/team-placeholder.png" alt="Person">
+                </figure>
+
+                <h3 class="OurTeam__name">dr. Jan <span>Kowalski</span></h3>
+
+                <p class="OurTeam__position">Dyrektor placówki</p>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php get_footer(); ?>
